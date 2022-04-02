4c68b8fefa69aed0b7476915c91bac94adab3b6f
Jeferson Luis Alves de Souza <jsouza@ibm.com>
2022-04-01 22:13:08 -0300

    .
    .

--- server/download.js
+++ server/download.js
@@ -3,19 +3,23 @@ const request = require('request');
 const path = require('path');
 
 let imgDownload = async function (uri, filename, callback) {
-  if (fs.existsSync(path.join(__dirname, '/../images/' + filename + '.png'))) {
-    console.log('arquivo existe');
-  } else {
-    await request.head(uri, function (err, res, body) {
-      request(uri)
-        .pipe(
-          fs.createWriteStream(
-            path.join(__dirname, '/../images/' + filename + '.png')
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(path.join(__dirname, '/../images/' + filename + '.png'))) {
+      resolve(true)
+    } else {
+      request.head(uri, async function (err, res, body) {
+        request(uri)
+          .pipe(
+            fs.createWriteStream(
+              path.join(__dirname, '/../images/' + filename + '.png')
+            )
           )
-        )
-        .on('close', callback);
-    });
-  }
+          .on('close', resolve(async function (e) { console.log(e) }));
+      });
+    }
+
+  })
+
 };
 
 module.exports = {
